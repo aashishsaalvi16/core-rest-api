ecffaa85ca0752f54eb4d88a011950826c5f2f00
ashish salvi <aashish.saalvi16@gmail.com>
2021-11-26 18:02:26 +0530

    Initial migration for Indentity Management pushed to DB.

---
+++ core-rest-api/Models/MyDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_rest_api.Models
+{
+    public class MyDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- core-rest-api/Startup.cs
+++ core-rest-api/Startup.cs
@@ -1,7 +1,10 @@
+using core_rest_api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,17 @@ namespace core_rest_api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContextPool<MyDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DbConnectionString")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>(
+        options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            //Other options go here
+        }
+        )
+    .AddEntityFrameworkStores<MyDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
